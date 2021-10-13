907a983aad872ae85fdc3368f72f876732200de8
dominick <dominickugas@gmail.com>
2021-10-12 21:21:43 -0400

    commit tree ef55f0e32acd707e0160d78a44fbf10f054eb666

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4
+
+let max = Math.max(18, 19, 20)
